a76183eb4ff44fa47db2c0282728fc9895b2b4f5
SoleneMarie <solene.plassart@gmail.com>
2025-07-03 11:52:59 +0200

    préparation au déploiement sur Northflank

--- config/plugins.js
+++ config/plugins.js
@@ -16,7 +16,9 @@ module.exports = ({ env }) => ({
       },
       actionOptions: {
         upload: {},
-        uploadStream: {},
+        uploadStream: {
+          timeout: 1000 * 60 * 3,
+        },
         delete: {},
       },
     },
